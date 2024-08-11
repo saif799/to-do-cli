8fb80af603d1ec186381260b59a4b5f304379703
saif799 <faysalbakziz@gmail.com>
2024-08-11 21:32:44 +0100

    created the write and read functions

--- cmd/zero.go
+++ cmd/zero.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 	"strconv"
+
+	"github.com/gocarina/gocsv"
 )
 
 func Add(first string, second string) (result string) {
@@ -63,4 +67,49 @@ func Divide(divide string, by string, shouldRoundUp bool) (result string, e erro
 		return fmt.Sprintf("%.2f", num1/num2), nil
 	}
 	return fmt.Sprintf("%f", num1/num2), nil
+}
+
+func Write(ID, Description, CreatedAt, IsComplete string) {
+
+	file, err := os.OpenFile("data.csv", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	data := []string{ID, Description, CreatedAt, IsComplete}
+
+	errr := writer.Write(data)
+	if errr != nil {
+		panic("this shit broke")
+	}
+
+}
+
+type Record struct {
+	ID          string `csv:"ID"`
+	Description string `csv:"Description"`
+	CreatedAt   string `csv:"CreatedAt"`
+	IsComplete  string `csv:"IsComplete"`
+}
+
+func Read() {
+	file, err := os.Open("data.csv")
+
+	if err != nil {
+		panic(err)
+	}
+
+	var records []Record
+
+	if err := gocsv.UnmarshalFile(file, &records); err != nil {
+		panic(err)
+	}
+
+	for _, record := range records {
+		fmt.Printf("ID: %s, Description: %s, IsComplete: %s \n", record.ID, record.Description, record.IsComplete)
+	}
+
+	defer file.Close()
 }
--- main.go
+++ main.go
@@ -4,5 +4,7 @@ import "github.com/saif799/to-do-cli/cmd"
 
 func main() {
 
-	cmd.Execute()
+	// cmd.Read()
+	cmd.Write("4", "the final test", "2024-08-27T16:45:19-05:00", "flase")
+	// cmd.Execute()
 }
